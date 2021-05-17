d1b507f1eaece078640da467c219d51017390ba0
Vivian Leung <vivz753@gmail.com>
2021-05-17 13:18:28 -0700

    Update Player.js

--- src/components/organisms/Splash2/Player.js
+++ src/components/organisms/Splash2/Player.js
@@ -15,7 +15,7 @@ const Player = ({ children, url}) => {
   return (
     <div className="relative" onClick={toggle}>
       <button className="bg-yellow-500 rounded-full w-20 h-20 sm:w-24 sm:h-24 md:w-32 md:h-32 border-yellow-700 border-b-8 hover:bg-yellow-600 focus:bg-yellow-700 focus:outline-none">
-        <div className="absolute text-center w-full inset-y-0 items-center flex justify-center text-yellow-700">
+        <div className="absolute text-center w-full inset-y-0 items-center flex justify-center text-yellow-800">
           {audioPlaying ? (<FaPause className="animate-spin-slow w-6 h-6 md:w-8 md:h-8"/>) : (<FaPlay className="w-6 h-6 md:w-8 md:h-8" />)}
         </div>
       </button>
