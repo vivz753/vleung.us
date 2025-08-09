593a05e2cc7031efa12b82664bbc95476fdf7683
Vivian Leung <vivz753@gmail.com>
2025-08-09 16:02:02 -0700

    Adjust footer for mobile screens

--- web/components/core/Layout/Footer.tsx
+++ web/components/core/Layout/Footer.tsx
@@ -21,7 +21,7 @@ const LogoLink: FC<{ link: string; label: string; icon: Icon }> = ({ link, label
 
 export const Footer: FC = () => {
   return (
-    <footer className="flex w-full flex-col items-center gap-4 bg-pink-200 px-12 py-10 lg:flex-row lg:px-24">
+    <footer className="flex w-full justify-center lg:justify-start items-center gap-4 bg-pink-200 px-12 py-10 flex-row lg:px-24">
       <LogoLink link="mailto:vivz753@gmail.com" label="" icon={Icon.MAIL} />
       <LogoLink link="https://github.com/vivz753" label="" icon={Icon.GITHUB} />
       <LogoLink link="https://linkedin.com/in/vivz753" label="" icon={Icon.LINKEDIN} />
